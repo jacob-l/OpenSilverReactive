b3ad9323046ce8f4fd0f8ca9d12ae11f958ed6f0
Iakov Lilo <jlilo@hwdtech.com>
2022-09-22 17:22:51 +0600

    Add project files.

---
+++ OpenSilverReactive/OpenSilverReactive.Simulator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using OpenSilver.Simulator;
+using System;
+
+namespace OpenSilverReactive.Simulator
+{
+    internal static class Startup
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            return SimulatorLauncher.Start(typeof(App));
+        }
+    }
+}
---
+++ OpenSilverReactive/OpenSilverReactive/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace OpenSilverReactive
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Enter construction logic here...
+            Observable
+                .Interval(TimeSpan.FromSeconds(1.0))
+                .Timestamp()
+                .Subscribe(RepeatAction);
+        }
+
+        private void RepeatAction(Timestamped<long> ts)
+        {
+            Console.WriteLine(ts.Timestamp);
+            TextBlock1.Text += Environment.NewLine + ts.Timestamp;
+        }
+    }
+}
